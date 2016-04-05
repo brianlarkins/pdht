4ad14b7086499c2a9157267c370ee8f4d34bcabf
D. Brian Larkins <larkinsb@rhodes.edu>
2016-04-04 22:47:33 -0500

    added barrier support

---
+++ test/barrier.c
@@ -0,0 +1,36 @@
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <pdht.h>
+
+
+extern pdht_context_t *c;
+
+int main(int argc, char **argv);
+
+int main(int argc, char **argv) {
+  pdht_t *ht;
+  
+  //printf("pid: %d\n", getpid());
+
+
+  ht = pdht_create();
+
+#if 0
+  printf("%d: before barrier\n", c->rank);
+  fflush(stdout);
+  pdht_barrier();
+  printf("%d: after barrier\n", c->rank);
+#endif
+
+
+  for (int i=0; i < c->size; i++) {
+    if (i == c->rank) {
+      printf("hello from %d of %d!\n", c->rank, c->size);
+      fflush(stdout);
+    } 
+    pdht_barrier();
+  }
+  
+  pdht_free(ht);
+}
