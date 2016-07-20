8332d68e18b1b96fff9dc2c7e1ca7f59afbb8a2e
D. Brian Larkins <larkinsb@rhodes.edu>
2016-07-20 16:07:54 -0500

    added collision/not found detection

--- test/collision.c
+++ test/collision.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv);
 void f_hash(pdht_t *dht, void *key, ptl_match_bits_t *mbits, ptl_process_t *rank) {
   // everything hashes to 1,1
   *mbits = 1;
-  *rank = 1;
+  (*rank).rank = 1;
 }
 
 
@@ -26,6 +26,7 @@ int main(int argc, char **argv) {
   pdht_t *ht;
   unsigned long key = 10;
   double pbuf[ASIZE], gbuf[ASIZE];
+  pdht_status_t ret;
   
   // create hash table
   ht = pdht_create(sizeof(unsigned long), ASIZE * sizeof(double), PdhtModeStrict);
@@ -37,21 +38,32 @@ int main(int argc, char **argv) {
     goto done;
   }
 
+  pdht_sethash(ht, f_hash);
+
 
   if (c->rank == 0) {
     for (int i=0; i<ASIZE; i++) {
-       pbuf[i] = i * 1.1;
-       gbuf[i] = i * 2.2;
+       pbuf[i] = i * 1.1 + 13.0;
+       gbuf[i] = i * 2.2 + 13.0;
     }
 
     printf("%d: putting object\n", c->rank);
-    pdht_put(ht, &key, pbuf);
+    ret = pdht_put(ht, &key, pbuf);
+   
+    if (ret != PdhtStatusOK) {
+      printf("uh-oh\n");
+    }
     pdht_barrier();
     pdht_barrier();
 
-    printf("%d: putting object\n", c->rank);
+    printf("%d: checking next object\n", c->rank);
     key += 1;
-    pdht_put(ht, &key, gbuf);
+    ret = pdht_get(ht, &key, gbuf);
+
+    if (ret != PdhtStatusCollision) {
+      printf("uh-oh %d\n", ret);
+    } 
+
     pdht_barrier();
 
 
